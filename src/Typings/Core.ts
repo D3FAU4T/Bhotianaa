e52290895bc162009a4a4f21b5ffa14cb5517c27
D3FAU4T <d3fau4t@gmail.com>
2023-09-28 16:56:40 +0530

    Added Core typings

---
+++ src/Typings/Core.ts
@@ -0,0 +1,19 @@
+import { ChatUserstate } from "tmi.js";
+import { Bhotianaa } from "../Core/Client";
+
+type RunOptions = {
+    Channel: string;
+    UserState: ChatUserstate;
+    Message: string;
+    Self: boolean;
+}
+
+export interface CommandsInterface {
+    Name: string;
+    Description: string;
+    Run: (Options: RunOptions, Client: Bhotianaa) => void;
+}
+
+export type AnnounceColors = 'blue' | 'green' | 'orange' | 'purple' | 'primary'
+
+export const LogErrorPath = './src/Logs/Error.log';
