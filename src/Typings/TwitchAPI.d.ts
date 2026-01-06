834bf41ed50d46e502ca7c283d0413e9608efa92
D3FAU4T <d3fau4t@gmail.com>
2026-01-06 08:41:03 +0530

    Updated Twitch API typings

--- src/Typings/TwitchAPI.d.ts
+++ src/Typings/TwitchAPI.d.ts
@@ -3,12 +3,12 @@ export interface TwitchUser {
     login: string;
     display_name: string;
     type: "",
-    broadcaster_type: "partner"
+    broadcaster_type: "partner" | "affiliate" | "";
     description: string;
     profile_image_url: string; // https://static-cdn.jtvnw.net/jtv_user_pictures/8a6381c7-d0c0-4576-b179-38bd5ce1d6af-profile_image-300x300.png
     offline_image_url: string; // https://static-cdn.jtvnw.net/jtv_user_pictures/3f13ab61-ec78-4fe6-8481-8682cb3b0ac2-channel_offline_image-1920x1080.png
     view_count: number;
-    email: string;
+    email?: string;
     created_at: string; // 2016-12-14T20:32:28Z
 }
 
