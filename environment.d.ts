94b2bc76966d0471b91c083f274432a7d1cf6ef6
D3FAU4T <d3fau4t@gmail.com>
2026-01-21 23:37:47 +0530

    Dev: Remove autocomplete for unused environment variables

--- environment.d.ts
+++ environment.d.ts
@@ -6,10 +6,6 @@ declare global {
             CSRF_SECRET: string;
             TWITCH_CLIENT_ID: string;
             TWITCH_CLIENT_SECRET: string;
-            TWITCH_USERNAME: string;
-            TWITCH_AUTH_URL: string;
-            TWITCH_CHANNEL: string;
-            TWITCH_CHANNEL_ID: string;
             DISCORD_WEBHOOK_URL: string;
         }
     }
